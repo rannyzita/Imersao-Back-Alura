c74fbc54c0a1c51398d618666ac0b9596785cfda
rannyzita <fs.vitoria.soares@gmail.com>
2024-11-21 08:53:41 -0300

    adicionando id na busca de post

--- instalike-back/server.js
+++ instalike-back/server.js
@@ -1,11 +1,42 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Gato fofo dormindo",
+        imagem: "https://placekitten.com/200/300"
+    }
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
 // rota
-app.get("/", (req, res) => {
-    res.status(200).send("Bem vindo a imersão backend...");
+app.get("/posts", (req, res) => {
+    // 200 = requisição bem sucedida
+    res.redirect("https://http.cat/200")
+    res.status(200).json(posts);
+});
+
+// criano um metodo especifico para buscar o id de algm post
+function buscarPostPorId(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    })
+};
+
+// :id quer dizer q vou acessar algm var id
+app.get("/posts/:id", (req, res) => {
+    // 200 = requisição bem sucedida
+    const index = buscarPostPorId(req.params.id);
+    res.status(200).json(posts[index]);
 });
