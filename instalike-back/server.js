1df5cf66868d3ab0700135ab661a18602d2ad7cf
rannyzita <fs.vitoria.soares@gmail.com>
2024-12-02 16:22:31 -0300

    criando post

--- instalike-back/server.js
+++ instalike-back/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "./src/routes/postsRoutes.js";
- 
+
 // cria uma instância do Express, que será utilizada para criar o servidor web.
 const app = express();
 routes(app);
@@ -11,15 +11,15 @@ app.listen(3000, () => {
 });
 
 // criando um metodo especifico para buscar o id de algm post
-function buscarPostPorId(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    })
-};
+// function buscarPostPorId(id) {
+//     return posts.findIndex((post) => {
+//         return post.id === Number(id);
+//     })
+// };
 
-// :id quer dizer q vou acessar algm var id
-app.get("/posts/:id", (req, res) => {
-    // 200 = requisição bem sucedida
-    const index = buscarPostPorId(req.params.id);
-    res.status(200).json(posts[index]);
-});
+// // :id quer dizer q vou acessar algm var id
+// app.get("/posts/:id", (req, res) => {
+//     // 200 = requisição bem sucedida
+//     const index = buscarPostPorId(req.params.id);
+//     res.status(200).json(posts[index]);
+// });
