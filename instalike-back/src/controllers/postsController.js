f538b32f7f4ddad449db4b4880fbe63f04a25b82
rannyzita <fs.vitoria.soares@gmail.com>
2024-12-02 07:29:25 -0300

    adicionando storage

--- instalike-back/src/controllers/postsController.js
+++ instalike-back/src/controllers/postsController.js
@@ -17,3 +17,16 @@ export async function postarNovoPost(req, res) {
         res.status(500).json({"Erro":"Falha na requisição."})
     }
 }
+
+export async function uploadImagem(req, res) {
+    const novoPost = req.body
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch (erro) {
+        console.error(erro.message);
+        // erro 500, erro interno no servidor  
+        res.status(500).json({"Erro":"Falha na requisição."})
+    }
+}
--- instalike-back/src/routes/postsRoutes.js
+++ instalike-back/src/routes/postsRoutes.js
@@ -1,5 +1,21 @@
 import express from "express";
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem} from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({dest:"./uploads", storage});
+
+// linux ou mac
+// const upload = multer({dest:"./uploads", storage});
+
 // verbos https
 // get(pegar), post(criar), delete, put(att)
 const routes = (app) => {
@@ -10,7 +26,10 @@ const routes = (app) => {
     // 1. Utiliza a função `getTodosPosts` para obter todos os posts.
     // 2. Envia uma resposta HTTP com status 200 (sucesso) e o array de posts no formato JSON.
     app.get("/posts", listarPosts);
-    app.post("/posts", postarNovoPost)
+    // rota para criar post
+    app.post("/posts", postarNovoPost);
+    // rota, em que recebe um unico arquivo
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
 export default routes;
